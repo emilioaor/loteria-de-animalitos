e7cef84c267c7604ba26779da8fef1efc69330e4
Emilio Ochoa <emilioaor@gmail.com>
2018-01-05 20:15:29 -0400

    Nombre de la agencia en el ticket

--- resources/views/text/ticket.blade.php
+++ resources/views/text/ticket.blade.php
@@ -1,10 +1,11 @@
 ----------------------------------
       LOTERIA DE ANIMALITOS
 ----------------------------------
+A.G 9192
 TICKET: {{ $ticket->public_id }}
 FECHA: {{ date_format($ticket->created_at, 'd-m-Y h:i a') }}
 SORTEOS:
-@foreach($ticket->dailySorts as $dailySort)
+@foreach($ticket->dailySorts()->orderBy('time_sort')->get() as $dailySort)
 {{ strtoupper($dailySort->sort->description . ' ' . $dailySort->timeSortFormat()) }}
 @endforeach
 
@@ -17,7 +18,7 @@ ANIMALITO                MONTO
 TOTAL                  {{ number_format($ticket->amount(), 2, ',', '.') }}
 
 REVISE SU TICKET
-VALIDO POR 5 DIAS
+VALIDO POR 3 DIAS
 
 
 ----------------------------------
