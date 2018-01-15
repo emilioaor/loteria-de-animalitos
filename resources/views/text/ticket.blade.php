69acd4196df870073100587a618e3b66a9dad3d7
Emilio Ochoa <emilioaor@gmail.com>
2018-01-15 19:32:10 -0400

    Recortando ticket

--- resources/views/text/ticket.blade.php
+++ resources/views/text/ticket.blade.php
@@ -1,25 +1,15 @@
-----------------------------------
-      LOTERIA DE ANIMALITOS
-----------------------------------
 {{ env('APP_LOCAL_NAME') }}
 TICKET: {{ $ticket->public_id }}
 FECHA: {{ date_format($ticket->created_at, 'd-m-Y h:i a') }}
-TAQUILLA: {{ $ticket->user->name }}
-SORTEOS:
 @foreach($ticket->dailySorts()->orderBy('time_sort')->get() as $dailySort)
 {{ strtoupper($dailySort->sort->description . ' ' . $dailySort->timeSortFormat()) }}
 @endforeach
-
-ANIMALITO                MONTO
-----------------------------------
+------------------------------
 @foreach($ticket->animals as $animal)
 {{ $animal->getLabelMoreSpace() }}{{ $animal->pivot->amount }}
 @endforeach
-----------------------------------
+------------------------------
 TOTAL                  {{ number_format($ticket->amount(), 2, ',', '.') }}
 
-REVISE SU TICKET
 VALIDO POR 3 DIAS
-
-
-----------------------------------
+------------------------------
