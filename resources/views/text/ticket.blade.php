5fd093b808edddb5f50095e796cb6b55b52d527e
Emilio Ochoa <emilioaor@gmail.com>
2018-01-21 00:15:46 -0400

    Nombre de agencia estatico por problemas de impresion de ticket

--- resources/views/text/ticket.blade.php
+++ resources/views/text/ticket.blade.php
@@ -1,4 +1,4 @@
-{{ env('APP_NAME') }}
+A.G 9192
 TICKET: {{ $ticket->public_id }}
 FECHA: {{ date_format($ticket->created_at, 'd-m-Y h:i a') }}
 @foreach($ticket->dailySorts()->orderBy('time_sort')->get() as $dailySort)
